64aeda990d06394012062711f8ad4b96d6148f96
Eric Marchand <eric.marchand@4d.com>
2019-11-08 11:48:37 +0100

    swiftlint autocorrect

--- Sources/Formatters/UILabel+html.swift
+++ Sources/Formatters/UILabel+html.swift
@@ -9,7 +9,7 @@ import UIKit
 import QMobileUI
 
 extension UILabel {
-    
+
     @objc dynamic var htmlText: String? {
         get {
             return self.text // N.B. no reverse transformation
@@ -18,11 +18,11 @@ extension UILabel {
             self.attributedText = newValue?.htmlToAttributedString
         }
     }
-    
+
 }
 
 fileprivate extension String {
-    
+
     var htmlToAttributedString: NSAttributedString {
         let data = Data(self.utf8)
         if let attributedString = try? NSAttributedString(data: data, options: [.documentType: NSAttributedString.DocumentType.html, .characterEncoding: String.Encoding.utf8.rawValue], documentAttributes: nil) {
