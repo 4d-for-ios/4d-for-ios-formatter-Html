aa29dd423225b946cd6f105fcf3fa870f919f7bd
Quentin Marciset <quentin.marciset@4d.com>
2022-02-03 09:52:26 +0100

    Revert "Use webview for html formatter #133958"
    This reverts commit 1081872ba523b3e5d2601e16e2125d44e2bcf6bc.

--- android/Formatters/HtmlText.kt
+++ android/Formatters/HtmlText.kt
@@ -1,19 +1,17 @@
 package ___PACKAGE___
 
-import android.view.View
-import android.view.ViewGroup
-import android.webkit.WebView
+import android.os.Build
+import android.text.Html
 import android.widget.TextView
+import androidx.core.text.HtmlCompat
 import androidx.databinding.BindingAdapter
 
 @BindingAdapter("htmlText")
 fun htmlText(view: TextView, htmlText: String?) {
     if (htmlText.isNullOrEmpty()) return
-
-    val webView = WebView(view.context)
-    webView.setPadding(view.paddingStart, view.paddingTop, view.paddingEnd, view.paddingBottom)
-    webView.layoutParams = view.layoutParams
-    webView.loadDataWithBaseURL(null, htmlText, "text/html", "utf-8", null)
-    (view.parent as ViewGroup?)?.addView(webView)
-    view.visibility = View.GONE
+    view.text = if (Build.VERSION.SDK_INT >= Build.VERSION_CODES.N) {
+        Html.fromHtml(htmlText, Html.FROM_HTML_MODE_COMPACT)
+    } else {
+        HtmlCompat.fromHtml(htmlText, HtmlCompat.FROM_HTML_MODE_LEGACY)
+    }
 }
